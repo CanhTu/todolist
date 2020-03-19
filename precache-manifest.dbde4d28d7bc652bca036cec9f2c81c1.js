f75b94b5f2580616b9af3f548a8ea064d9741160
CanhTu <canhtu1007@gmail.com>
2020-03-19 16:48:02 +0700

    Updates

---
+++ precache-manifest.dbde4d28d7bc652bca036cec9f2c81c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d711691efb5da721f2365eb080a449",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "1e0292a4346fb2775122",
+    "url": "/todolist/static/css/main.dab9696b.chunk.css"
+  },
+  {
+    "revision": "f836de6fa305de6766f7",
+    "url": "/todolist/static/js/2.54d72e2f.chunk.js"
+  },
+  {
+    "revision": "1e0292a4346fb2775122",
+    "url": "/todolist/static/js/main.32dd0b02.chunk.js"
+  },
+  {
+    "revision": "494ea65bfa3a7b566acf",
+    "url": "/todolist/static/js/runtime~main.b0077857.js"
+  },
+  {
+    "revision": "dcffa1771d352a5c1e1623b7db90af1b",
+    "url": "/todolist/static/media/notask.dcffa177.jpg"
+  }
+]);
